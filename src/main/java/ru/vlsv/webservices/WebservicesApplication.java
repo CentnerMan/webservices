adcf81c30285a002b90ab9aae263dcd1ca7ef63a
CentnerMan <44133485+CentnerMan@users.noreply.github.com>
2020-07-14 12:40:26 +0300

    clean

--- src/main/java/ru/vlsv/webservices/WebservicesApplication.java
+++ src/main/java/ru/vlsv/webservices/WebservicesApplication.java
@@ -1,13 +1,13 @@
-package ru.vlsv.nostromo;
+package ru.vlsv.webservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NostromoApplication {
+public class WebservicesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NostromoApplication.class, args);
+		SpringApplication.run(WebservicesApplication.class, args);
 	}
 
 }
